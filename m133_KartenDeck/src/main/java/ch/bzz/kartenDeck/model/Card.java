cd0a8cb760897ae6bbbc033a94eb90861ae8ba7a
JonasR <rhbaryj@bzz.ch>
2022-06-13 21:23:31 +0200

    working on it

--- m133_KartenDeck/src/main/java/ch/bzz/kartenDeck/model/Card.java
+++ m133_KartenDeck/src/main/java/ch/bzz/kartenDeck/model/Card.java
@@ -2,6 +2,13 @@ package ch.bzz.kartenDeck.model;
 
 import ch.bzz.kartenDeck.data.DataHandler;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import javax.ws.rs.FormParam;
+import java.util.List;
+import java.util.HashMap;
+import java.util.UUID;
+import javax.ws.rs.FormParam;
 import java.math.*;
 import java.lang.*;
 
@@ -10,14 +17,148 @@ import java.lang.*;
  */
 public class Card {
     @JsonIgnore
+    @FormParam("deck")
+    @Size(min = 1, max = 10, message = "Your Decks name must be between 1 and 10 characters")
     private Deck deck;
 
+    @FormParam("cardUUID")
+    @Pattern(regexp = "|[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}", message = "The Cards UUID must be a valid UUID")
     private String cardUUID;
+
+    @FormParam("card")
+    @Size(min = 1, max = 50, message = "Your Cards name must be between 1 and 50 characters")
     private String card;
+
+    @FormParam("monstertype")
+    @Size(min = 1, max = 50, message = "The Monstertype must be between 1 and 10 characters")
     private String monstertype;
+
+    @FormParam("ATK")
+    @Size(min = 1, max = 10, message = "Your Cards ATK-stats must be between 1 and 10 characters")
     private BigDecimal ATK;
+
+    @FormParam("DEF")
+    @Size(min = 1, max = 10, message = "Your Cards DEF-stats must be between 1 and 10 characters")
     private BigDecimal DEF;
+
+    @FormParam("ability")
+    @Size(min = 1, max = 50, message = "Your Cards ability must be between 1 and 50 characters")
     private String ability;
+
+    /**
+     * constructor with all params
+     * @param deck
+     * @param cardUUID
+     * @param card
+     * @param monstertype
+     * @param ATK
+     * @param DEF
+     * @param ability
+     * @throws IllegalArgumentException
+     */
+    public Card(Deck deck, String cardUUID, String card, String monstertype, BigDecimal ATK, BigDecimal DEF, String ability) throws IllegalArgumentException{
+        setDeck(deck);
+        setCardUUID(cardUUID);
+        setCard(card);
+        setMonstertype(monstertype);
+        setATK(ATK);
+        setDEF(DEF);
+        setAbility(ability);
+    }
+
+    /**
+     * constructor w/o deck
+     * @param cardUUID
+     * @param card
+     * @param monstertype
+     * @param ATK
+     * @param DEF
+     * @param ability
+     * @throws IllegalArgumentException
+     */
+    public Card(String cardUUID, String card, String monstertype, BigDecimal ATK, BigDecimal DEF, String ability) throws IllegalArgumentException{
+        setCardUUID(cardUUID);
+        setCard(card);
+        setMonstertype(monstertype);
+        setATK(ATK);
+        setDEF(DEF);
+        setAbility(ability);
+    }
+
+    /**
+     * constructor w/o deck, cardUUID
+     * @param card
+     * @param monstertype
+     * @param ATK
+     * @param DEF
+     * @param ability
+     * @throws IllegalArgumentException
+     */
+    public Card(String card, String monstertype, BigDecimal ATK, BigDecimal DEF, String ability) throws IllegalArgumentException{
+        setDeck(deck);
+        setCardUUID(cardUUID);
+        setCard(card);
+        setMonstertype(monstertype);
+        setATK(ATK);
+        setDEF(DEF);
+        setAbility(ability);
+    }
+
+    /**
+     * constructor w/o deck, cardUUID, card
+     * @param monstertype
+     * @param ATK
+     * @param DEF
+     * @param ability
+     * @throws IllegalArgumentException
+     */
+    public Card(String monstertype, BigDecimal ATK, BigDecimal DEF, String ability) throws IllegalArgumentException{
+        setMonstertype(monstertype);
+        setATK(ATK);
+        setDEF(DEF);
+        setAbility(ability);
+    }
+
+    /**
+     * constructor w/o deck, cardUUID, card, monstertype
+     * @param ATK
+     * @param DEF
+     * @param ability
+     * @throws IllegalArgumentException
+     */
+    public Card(BigDecimal ATK, BigDecimal DEF, String ability) throws IllegalArgumentException{
+        setATK(ATK);
+        setDEF(DEF);
+        setAbility(ability);
+    }
+
+    /**
+     * constructor w/o deck, cardUUID, card, monstertype, ATK
+     * @param DEF
+     * @param ability
+     * @throws IllegalArgumentException
+     */
+    public Card(BigDecimal DEF, String ability) throws IllegalArgumentException{
+        setDEF(DEF);
+        setAbility(ability);
+    }
+
+    /**
+     * constructor w/o deck, cardUUID, card, monstertype, ATK, DEF
+     * @param ability
+     * @throws IllegalArgumentException
+     */
+    public Card(String ability) throws IllegalArgumentException{
+        setAbility(ability);
+    }
+
+    /**
+     * empty constructor, needed for JSON i suppose
+     */
+    public Card(){
+        setCardUUID(UUID.randomUUID().toString());
+    }
+
 
     /**
      * gets the deckUUID from the Deck-object
@@ -67,11 +208,13 @@ public class Card {
     }
 
     /**
-     * sets cardUUID
+     * sets random cardUUID with help of the UUID creator
      *
      * @param cardUUID the value to set
      */
-    public void setCardUUID(String cardUUID) {
+    public void setCardUUID(String cardUUID) throws IllegalArgumentException {
+        this.cardUUID = UUID.randomUUID().toString();
+        UUID.fromString(cardUUID);
         this.cardUUID = cardUUID;
     }
 
@@ -165,5 +308,13 @@ public class Card {
      */
     public void setAbility(String ability) {
         this.ability = ability;
+    }
+
+    /**
+     *
+     */
+    public void addDeck(Deck deck){
+        if (deck == null){
+        }
     }
 }
